6991d17a58aa3084ef337aee0767203ce954c287
Ankit <ankitcool4370@gmail.com>
2022-11-07 12:33:29 +0530

    basicprogram

---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee obj2 = new employee();
+            obj2.display();
+            obj2.show();
+            Console.ReadLine();
+        }
+    }
+    public class cls
+    {
+        public void display()
+        {
+            Console.WriteLine("HELLO");
+        }
+    }
+    public class employee : cls
+    {
+        public void show()
+        {
+            Console.WriteLine("welcome");
+        }
+    }
+}
